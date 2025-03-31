f1924ae15c9369a53f667a5ec58a198914b4c999
ACaponpon03 <caponponangeline003@gmail.com>
2025-03-31 17:29:33 +0800

    update images

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
-// Import the express module
 const express = require('express');
 
-// Create an instance of the express app
 const app = express();
 
-// Set up a basic route for the homepage (root route)
 app.get('/', (req, res) => {
   res.send('Welcome to the Conference!');
 });
 
-// Start the server and listen on port 8080
 app.listen(8080, () => {
   console.log('Server is running on http://localhost:8080');
 });
